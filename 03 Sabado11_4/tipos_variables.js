f0309c9f9854957bd33645c0b707c512e3e733fb
Jose Quesada <josequesada.edu@gmail.com>
2020-04-11 12:46:09 +0200

    al acabar la clase del sab11_4

--- 03 Sabado11_4/tipos_variables.js
+++ 03 Sabado11_4/tipos_variables.js
@@ -2,9 +2,12 @@ let a=15;
 console.log(a);
 console.log(typeof a); // tipo de variable
 a="Cadena";
+console.log(a);
 console.log(typeof a);
 a=[12,23,4]; //definimos array
+console.log(a);
 console.log(typeof a);
-console.log(a instanceof Array);
+//console.log(a instanceof Array);  //devuelve true porque a es de tipo Array
+console.log(a instanceof String);   //devuelve falso
 let b;
-console.log(typeof b);
+console.log(typeof b);  //devuelve que b es de tipo undefined
