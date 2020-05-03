a8dc8e43f8f097692f04f8ec978249ca5a90f99a
Jose Quesada <josequesada.edu@gmail.com>
2020-05-03 23:57:34 +0200

    Modificaciones DOM 25_4

--- 06 DOM 25_4/5 DomImages.js
+++ 06 DOM 25_4/5 DomImages.js
@@ -1,15 +1,16 @@
 /* Visualización del número de imagen(es) del documento */
 document.write("Número de imagen(es) del documento : " + document.images.length + "<br />");
+console.log(document.images);
 
 /* Visualización identificador de la primera imagen */
-document.write("Identificador de la primera imagen : " + document.images[0].id + "<br />");
+document.write("Atributo ID de la primera imagen : " + document.images[0].id + "<br />");
 
 /* Visualización del atributo border de la segunda imagen */
-document.write("Atributo border de la segunda imagen : " + document.images[1].border + "<br />");
+document.write("Atributo BORDER de la segunda imagen : " + document.images[1].border + "<br />");
 
 /* Visualización del atributo src de la tercera imagen */
-document.write("Atributo src de la tercera imagen : " + document.images[2].src + "<br />");
+document.write("Atributo SRC de la tercera imagen : " + document.images[2].src + "<br />");
 
 /* Visualización de los atributos width y height de la cuarta imagen */
-document.write("Atributo width de la cuarta imagen : " + document.images[3].width + "<br />");
-document.write("Atributo height de la cuarta imagen : " + document.images[3].height + "<br />");
+document.write("Atributo WIDTH de la cuarta imagen : " + document.images[3].width + "<br />");
+document.write("Atributo HEIGHT de la cuarta imagen : " + document.images[3].height + "<br />");
